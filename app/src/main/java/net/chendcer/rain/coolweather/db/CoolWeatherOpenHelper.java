efcf0c0b19e5915f39b9e2a5e8728c290ee3e703
陈*超 <547639151@qq.com>
2016-03-08 18:14:45 +0800

    Finish create table

--- app/src/main/java/net/chendcer/rain/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/net/chendcer/rain/coolweather/db/CoolWeatherOpenHelper.java
@@ -14,14 +14,26 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
         super(context, name, factory, version);
     }
 
-    public static final String CREATE_PROVINCE = "create table province("
+    public static final String CREATE_PROVINCE = "create table Province("
             + "id inteager primary key autoincrement,"
-            + "province_name text"
-            + "province_code text";
+            + "province_name text,"
+            + "province_code text)";
+    public static final String CREATE_CITY = "create table City("
+            + "id inteager primary key autoincrement,"
+            + "city_name text,"
+            + "city_code text,"
+            + "province_id integer)";
+    public static final String CREATE_COUNTY = "create table County("
+            + "id inteager primary key autoincrement,"
+            + "county_name text,"
+            + "county_code text,"
+            + "county_id integer,)";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+    db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
     }
 
     @Override
