b29a9477a9878a908fd59ea6c0074bb6add30c9e
陈*超 <547639151@qq.com>
2016-03-08 23:34:55 +0800

    create DB 80%

---
+++ app/src/main/java/net/chendcer/rain/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package net.chendcer.rain.coolweather.model;
+
+/**
+ * Created by chend on 2016/3/8.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
